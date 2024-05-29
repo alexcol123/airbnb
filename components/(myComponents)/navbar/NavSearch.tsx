5b950d6c0952e6d0a0e9e5e3bdead12cfb397598
Henry Munoz <alexcol123@yahoo.com>
2024-05-29 11:08:35 -0400

    chore: Remove unused import and fix search URL in NavSearch component

--- components/(myComponents)/navbar/NavSearch.tsx
+++ components/(myComponents)/navbar/NavSearch.tsx
@@ -1,14 +1,14 @@
 'use client'
 
 import { Input } from "../../ui/input"
-import { useSearchParams, usePathname, useRouter } from 'next/navigation'
+import { useSearchParams, useRouter } from 'next/navigation'
 import { useDebouncedCallback } from 'use-debounce'
 import { useState, useEffect } from 'react'
 
 
 const NavSearch = () => {
   const searchParams = useSearchParams()
-  const pathname = usePathname()
+
   const { replace } = useRouter()
 
   const [search, setsearch] = useState(searchParams.get('search')?.toString() || '')
@@ -24,7 +24,7 @@ const NavSearch = () => {
     } else {
       params.delete('search')
     }
-    replace(`${pathname}?${params.toString()}`)
+    replace(`/?${params.toString()}`)
   }
     , 600)
 
