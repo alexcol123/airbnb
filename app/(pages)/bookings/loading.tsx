c0be2f2159acc6f719bc2bd629248fb16ea303ae
Henry Munoz <alexcol123@yahoo.com>
2024-05-31 14:01:34 -0400

    feat: Add loading component for bookings page
    The code changes introduce a new file `loading.tsx` under the `app/(pages)/bookings` directory. This file imports the `LoadingTable` component from `@/components/(myComponents)/booking/LoadingTable` and renders it within a `div` element. The purpose of this change is to display a loading indicator while the bookings page is being loaded.
    
    Based on the recent user commits and repository commits, the commit message follows the convention of starting with a verb in the imperative form, followed by a concise description of the changes made.

---
+++ app/(pages)/bookings/loading.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import LoadingTable from "@/components/(myComponents)/booking/LoadingTable";
+
+
+
+function loading() {
+  return <div className="mt-20">
+
+    <LoadingTable />
+  </div>;
+}
+
+export default loading;
